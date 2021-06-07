20cc6cd08b6312cba0f41da06e27850ec2b7a8a1
Niket <niksbharai.1@gmail.com>
2021-06-07 22:40:22 +0530

    database paired

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express, { urlencoded } from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
 import morgan from "morgan";
 import mongoose from "mongoose";
 
@@ -11,7 +12,21 @@ const db = process.env.DB_URL;
 // MiddleWares
 app.use(urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 app.use(morgan("tiny"));
+
+// Database Setup
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database Paired Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(() => {
   console.log(`App is running at port ${port}`);

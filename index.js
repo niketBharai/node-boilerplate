7fe59754d88fe0fd4db199b88fc620ec7788d772
Niket <niksbharai.1@gmail.com>
2021-06-07 22:33:28 +0530

    server setup

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("node works fine");
+import express, { urlencoded } from "express";
+const app = express();
+import dotenv from "dotenv";
+dotenv.config();
+import morgan from "morgan";
+import mongoose from "mongoose";
+
+const port = process.env.PORT;
+const db = process.env.DB_URL;
+
+// MiddleWares
+app.use(urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.listen(() => {
+  console.log(`App is running at port ${port}`);
+});
